7f3b14befad06e5ff63e79756c74c09033eccfe3
Raul Acuna <raeioul@gmail.com>
2026-01-08 19:54:54 -0400

    Gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/pizzatime/',
   test: {
     globals: true,          // <-- gives you describe/it/expect
     environment: 'jsdom',   // <-- simulates browser for React components
