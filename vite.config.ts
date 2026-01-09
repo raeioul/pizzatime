101f43774ccb0622c4377c5aacf010b361ca56dd
Raul Acuna <raeioul@gmail.com>
2026-01-08 20:20:54 -0400

    Add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  base: mode === 'production' ? '/pizzatime/' : '/',
   test: {
     globals: true,          // <-- gives you describe/it/expect
     environment: 'jsdom',   // <-- simulates browser for React components
     setupFiles: './tests/setup.ts'
   },
-});
+}));
